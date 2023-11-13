1aa9cc21425b63392c20943274f803c68d6fb43d
afroraydude <thomasray0@gmail.com>
2023-11-13 16:46:33 -0500

    yes pages

--- pages/projects/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Container, Title } from "@mantine/core";
-
-export default function Projects({}) {
-  return (
-    <Container>
-      <Title order={1}>Projects</Title>
-    </Container>
-  )
-}
